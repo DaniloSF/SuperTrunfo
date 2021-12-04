ea9253e6a0eabac3b5ca46019ebdd718d2c568e7
Victor Hugo Bernardino <vhugoz1@hotmail.com>
2021-12-04 18:13:41 -0300

    Mensagem aleatória na tela final

--- Assets/Scripts/EndManager.cs
+++ Assets/Scripts/EndManager.cs
@@ -10,20 +10,44 @@ using UnityEngine.UI;
 
 public class EndManager : MonoBehaviour
 {
+    // Mensagens aleatórias para aparecer na tela de vitória e derrota!
+    string[] LoseMessages = new string[]
+    {
+        "\"CPU: Hoje é o seu dia de comer peixe!\"",
+        "\"CPU: Fui eu que coloquei aquela larva no seu almoço!\"",
+        "\"CPU: Você devia ter ido comprar um salgado!\"",
+        "\"CPU: Furei a sua fila, amigo!\""
+    };
+
+    string[] WinMessages = new string[]
+    {
+        "\"PLAYER: É isso que chamam de 'inteligência' artificial?\"",
+        "\"PLAYER: Esqueceram de descomentar seu código!\"",
+        "\"PLAYER: Agora eu posso comer em paz o meu almoço!\"",
+        "\"PLAYER: Ops! Apertei Alt + F4!\""
+    };
+
+    public GameObject Tela;
     // Start is called before the first frame update
     void Start()
     {
+        
+        Tela = GameObject.Find("Main Camera");
         if (VencedorGlobal.isWinner)
         {
+            Tela.GetComponent<Camera>().backgroundColor = new Color(0f, 0f, 0.5f);
             AudioSource VictorySong = GameObject.Find("VictorySong").GetComponent<AudioSource>();
             VictorySong.Play();
             GetComponent<Text>().text = "Parabéns! Você venceu!";
+            GameObject.Find("Mensagem").GetComponent<Text>().text = WinMessages[(int)Random.Range(0, WinMessages.Length)];
         }
         else
         {
+            Tela.GetComponent<Camera>().backgroundColor = new Color(0.7f, 0f, 0f);
             AudioSource DefeatSong = GameObject.Find("DefeatSong").GetComponent<AudioSource>();
             DefeatSong.Play();
-            GetComponent<Text>().text = "A CPU te derrotou, que pena!";
+            GetComponent<Text>().text = "A CPU te derrotou, que pena!\n";
+            GameObject.Find("Mensagem").GetComponent<Text>().text = LoseMessages[(int)Random.Range(0, WinMessages.Length)];
         }
     }
 
